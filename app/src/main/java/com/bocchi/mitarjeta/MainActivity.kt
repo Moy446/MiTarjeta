104ca729d3975d1fa1636dd6e4ce7d7e2b32254c
rodri <rodmin452@gmail.com>
2025-03-28 20:07:49 -0600

    C#2 Creation of NavController, update colors, end the LoginView, missing  finished the logic behind the buttons,

--- app/src/main/java/com/bocchi/mitarjeta/MainActivity.kt
+++ app/src/main/java/com/bocchi/mitarjeta/MainActivity.kt
@@ -6,39 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.bocchi.mitarjeta.formfield.FormField
-import com.bocchi.mitarjeta.ui.theme.MiTarjetaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MiTarjetaTheme {
-                FormField(Modifier, label = "Prueba")
-            }
+            MyAppNavigation()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MiTarjetaTheme {
-        Greeting("Android")
-
     }
 }
---
+++ app/src/main/java/com/bocchi/mitarjeta/MyAppNavigation.kt
@@ -0,0 +1,24 @@
+package com.bocchi.mitarjeta
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MyAppNavigation() {
+    // Aquí puedes definir la lógica de navegación y las pantallas de tu aplicación
+    // Por ejemplo, puedes usar un NavHost para gestionar la navegación entre diferentes pantallas
+    // y pasar los parámetros necesarios a cada pantalla.
+    // Puedes utilizar la biblioteca de navegación de Jetpack Compose para facilitar esto.
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+    ) {
+        composable("login") { LoginView(navController) }
+        //composable("home") { HomeView(navController) }
+        // Agrega más destinos según sea necesario
+    }
+}
