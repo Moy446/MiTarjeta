6181acc45a01c6b9d11e73f20e5e6ea80f691e54
ELQUESEAYA <mariana.yaya.12@gmail.com>
2025-04-21 22:16:43 -0600

    Citas y Vinculacion
    En teoria la parte visual del calendario (sin animaciones) y el intento de la parte visual de vinculacion, esta ultima tiene varios errores (probablemente de llaves)

--- app/src/main/java/com/bocchi/mitarjeta/ui/designs/Calendario.kt
+++ app/src/main/java/com/bocchi/mitarjeta/ui/designs/Calendario.kt
@@ -1,18 +1,38 @@
 package com.bocchi.mitarjeta.ui.designs
 
 import android.graphics.Color.WHITE
+import android.graphics.Color.toArgb
+//import android.graphics.Paint
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bocchi.mitarjeta.CalendarInput
@@ -22,16 +42,60 @@ import org.intellij.lang.annotations.JdkConstants.CalendarMonth
 private const val CALENDAR_ROWS = 5
 private const val CALENDAR_COLUMNS = 7
 
+
+class Calendario : ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContent{
+            val calendarInputList by  remember{
+                mutableStateOf(createCalendarList())
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0x57AD96)), //REVISAR
+                contentAlignment = Alignment.TopCenter
+            ){
+                Calendar(
+                    calendarImput = calendarInputList,
+                    onDayClick = {
+
+                    },
+                    month = "Abril", //MODIFICAR
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth()
+                        .aspectRatio(1.3f)
+                )
+            }
+        }
+    }
+    private fun createCalendarList() : List<CalendarInput>{
+        val calendarImputs = mutableListOf<CalendarInput>()
+        for (i in 1 .. 31){
+            calendarImputs.add(
+                CalendarInput(
+                    i,
+                    toDos = listOf(
+                        "Day $i:",
+                        "2p.m. Cita"
+                    )
+                )
+            )
+        }
+        return calendarImputs
+    }
+}
 @Composable
 fun Calendar(modifier: Modifier = Modifier,
              calendarImput: List<CalendarInput>,
              onDayClick : (Int)->Unit,
              strokeWidth:Float = 15f,
-             month: String){
-    Column (
+             month: String) {
+    Column(
         modifier = Modifier,
         verticalArrangement = Arrangement.spacedBy(10.dp)
-    ){
+    ) {
         Text(
             text = month,
             fontWeight = FontWeight.SemiBold,
@@ -40,11 +104,11 @@ fun Calendar(modifier: Modifier = Modifier,
         )
         Canvas(
             modifier = Modifier.fillMaxSize()
-        ){
-            val canvasHeigth= size.height
+        ) {
+            val canvasHeigth = size.height
             val canvasWeight = size.width
-            val ysteps = canvasHeigth/ CALENDAR_ROWS
-            val xsteps = canvasWeight/ CALENDAR_COLUMNS
+            val ySteps = canvasHeigth / CALENDAR_ROWS
+            val xSteps = canvasWeight / CALENDAR_COLUMNS
 
             drawRoundRect(
                 color = Color(0xFF3058B6),//Color
@@ -53,6 +117,58 @@ fun Calendar(modifier: Modifier = Modifier,
                     width = strokeWidth
                 )
             )
+            for (i in 1 until CALENDAR_ROWS) {
+                drawLine(
+                    color = Color(0xFF3058B6),//Color
+                    start = Offset(0f, ySteps*i),
+                    end = Offset(canvasWeight, ySteps*i),
+                    strokeWidth = strokeWidth
+                )
+            }
+            for (i in 1 until CALENDAR_COLUMNS) {
+                drawLine(
+                    color = Color(0xFF3058B6),//Color
+                    start = Offset(xSteps*i, 0f),
+                    end = Offset(xSteps*i, canvasHeigth),
+                    strokeWidth = strokeWidth
+                )
+            }
+            val textHeight = 17.dp.toPx()
+            for(i in calendarImput.indices){
+                val textPositionX = xSteps * (i% CALENDAR_COLUMNS) + strokeWidth
+                val textPositionY = (i / CALENDAR_COLUMNS) * ySteps + textHeight + strokeWidth/2
+                drawContext.canvas.nativeCanvas.apply {
+                    drawText(
+                        "${i+1}",
+                        textPositionX,
+                        textPositionY,
+                        android.graphics.Paint().apply {
+                            textSize = textHeight
+                            //color = Color.White//Color
+                           // color = white.toArgb
+                            isFakeBoldText = true
+                        }
+                    )
+                }
+            }
         }
     }
+}
+
+@Preview
+@Composable
+private fun createCalendarList() : List<CalendarInput>{
+    val calendarImputs = mutableListOf<CalendarInput>()
+    for (i in 1 .. 31){
+        calendarImputs.add(
+            CalendarInput(
+                i,
+                toDos = listOf(
+                    "Day $i:",
+                    "2p.m. Cita"
+                )
+            )
+        )
+    }
+    return calendarImputs
 }
--- app/src/main/java/com/bocchi/mitarjeta/ui/designs/VinculacionView.kt
+++ app/src/main/java/com/bocchi/mitarjeta/ui/designs/VinculacionView.kt
@@ -1,15 +1,173 @@
 package com.bocchi.mitarjeta.ui.designs
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.bocchi.mitarjeta.R
+import com.bocchi.mitarjeta.navigation.NavItemList
+import com.bocchi.mitarjeta.navigation.NavItemList.navItemList
+import com.bocchi.mitarjeta.ui.theme.MiTarjetaTheme
+import com.bocchi.mitarjeta.ui.theme.backgroud
 
 @Composable
-fun VinculacionView(navController: NavController){
-    var UID by rememberSaveable   { mutableStateOf("") }
-        Box()
+fun VinculacionView(navController: NavController){ //navController: controlador de navegación entre pantallas.
+
+    var UIDTarjeta by rememberSaveable { mutableStateOf("") }
+
+    //menu variables
+    var selectedRoute = remember { mutableStateOf("Vinculo") }
+
+    MiTarjetaTheme {
+        Scaffold(bottomBar = {
+            //Impresion del Menu
+            menuView(
+                navItemList = NavItemList.navItemList,
+                selectedView = selectedRoute.value,
+                onItemSelected = { titulo ->
+                    selectedRoute.value = titulo
+                    navController.navigate(titulo)
+                },
+            )
+        }
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(backgroud)
+                    .padding(top = 40.dp, bottom = 50.dp)
+            ) {
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(bottom = 80.dp)
+                        .align(Alignment.Center)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    Row {
+                        botonBack(
+                            Modifier
+                                .padding(start = 30.dp, end = 50.dp, top = 15.dp)
+                                .clickable {
+                                    navController.popBackStack()
+                                })
+                        textTittle(
+                            Modifier
+                                .width(170.dp)
+                                .height(52.dp),
+                            "Recargas"
+                        )
+                    }
+
+                    textDescription(
+                        Modifier
+                            .width(329.dp)
+                            .height(26.dp)
+                            .align(Alignment.CenterHorizontally),
+                        "Proporciona los siguientes datos",
+                    )
+                    ReadOnlyTextField(uid)
+
+                    selectedMonto.value = seleccionMontoRecargas()
+
+                    seleccionarTarjetaRecarga(
+                        tarjetasList = tarjetas,
+                        tarjetaSeleccionada = tarjetaSeleccionada,
+                        onSeleccionarTarjeta = { tarjetaSeleccionada = it },
+                        onAgregarTarjeta = { agregarTarjeta.value = !agregarTarjeta.value }
+                    )
+
+                    if (agregarTarjeta.value) {
+                        agegarTarjetaRecarga(true) { nuevaTarjeta ->
+                            if (!tarjetasDebitoList.contains(nuevaTarjeta)) {
+                                tarjetasDebitoList.add(nuevaTarjeta)
+                                var numero = nuevaTarjeta.numeroTarjeta
+                                numero = numero.reversed()
+                                numero = "**${numero[3]}${numero[2]}${numero[1]}${numero[0]}"
+                                tarjetas.add(numero)
+                                tarjetaSeleccionada = numero
+                            }
+                            agregarTarjeta.value = false
+                        }
+                    }
+
+                    mostrarMontosRecarga(selectedMonto.value)
+
+                    //botton pagar
+                    Button(modifier = Modifier
+                        .padding(vertical = 5.dp)
+                        .width(301.dp)
+                        .height(42.dp)
+                        .align(Alignment.CenterHorizontally),
+                        shape = RoundedCornerShape(20.dp),
+                        enabled = tarjetaSeleccionada != "------",
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFFE9762B)
+                        ),
+                        onClick = {
+                            /*TODO
+                                una tarjeta la cual va a pagar*/
+                        }) {
+                        Text(
+                            text = "Pagar",
+                            style = TextStyle(
+                                fontSize = 20.sp,
+                                fontFamily = FontFamily(Font(R.font.relay_niramit_medium)),
+                                fontWeight = FontWeight(500),
+                                color = Color(0xFF3058B6),
+
+                                )
+                        )
+                    }
+                }
+                //Impresion del Menu
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(106.dp)
+                        .align(Alignment.BottomCenter)
+                ) {
+                }
+            }
+        }
+    }
+
+   /*
+    menuView(
+        navItemList = navItemList,
+        selectedView = selectedRoute,
+        onItemSelected = { nuevoItem -> onItemSelected = nuevoItem
+    ) { }
+
+    */
 }
